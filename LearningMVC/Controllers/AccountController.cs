115e5425fe15eeddc84236ad06047b99df44d821
Ody Mbegbu <odytrice@gmail.com>
2015-10-13 18:52:07 +0100

    Added User Identity Management

--- LearningMVC/Controllers/AccountController.cs
+++ LearningMVC/Controllers/AccountController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using LearningMVC.Models;
+using LearningMVC.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,16 +13,34 @@ namespace LearningMVC.Controllers
 {
     public class AccountController : Controller
     {
+
+        private IAuthenticationManager Authentication => HttpContext.GetOwinContext().Authentication;
+
         // GET: Account
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
 
-        public ActionResult Register(ApplicationUser model) {
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            var usermanager = new UserManager<UserModel>(new UserStore());
+            var identity = usermanager.CreateIdentity(model, DefaultAuthenticationTypes.ApplicationCookie);
+            Authentication.SignIn(identity);
+            return View(model);
+        }
+
+        public ActionResult Register(UserModel model) {
 
             //TODO  :implement some validation
-            return View(moel);
+            return View(model);
+        }
+
+        public ActionResult SignOut()
+        {
+            Authentication.SignOut();
+            return RedirectToAction("login");
         }
     }
 }
---
+++ LearningMVC/Security/UserStore.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using LearningMVC.Models;
+
+namespace LearningMVC.Security
+{
+    public class UserStore : IUserStore<UserModel>, IUserRoleStore<UserModel>
+    {
+
+
+        public Task AddToRoleAsync(UserModel user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserModel> FindByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserModel> FindByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> GetRolesAsync(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsInRoleAsync(UserModel user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFromRoleAsync(UserModel user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
